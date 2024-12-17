4f84294751a4918407e9b34cd81c479e5a891168
Tony Pham <tony.pham@tc.gc.ca>
2024-12-17 15:57:24 -0500

    Finish day two part one :)

--- dayTwo/redNosedReport.py
+++ dayTwo/redNosedReport.py
@@ -1,3 +1,10 @@
+def load_reports_from_file(file_name: str, separator: str) -> list[list[int]]:
+    reports = []
+    with open(file_name, 'r', encoding = 'utf-8') as file_handler:
+        for report in file_handler:
+            reports.append([int(val) for val in report.split(' ')])
+    return reports
+
 def __is_adjacent_levels_diff_safe(level: int, adjacent_level: int, min_diff: int, max_diff: int) -> bool:
     diff = abs(level - adjacent_level)
     return diff >= min_diff and diff <= max_diff
@@ -26,5 +33,12 @@ def get_num_safe_reports(reports: list[list[int]], min_diff: int, max_diff: int)
             num_safe_reports += 1
     return num_safe_reports
 
+def __run():
+    file_name = input('Please enter the name of the file containing the report data: ')
+    reports = load_reports_from_file(file_name, ' ')
+    num_safe_reports = get_num_safe_reports(reports, 1, 3)
+    print('How many reports are safe?')
+    print(f'Answer: {num_safe_reports}')
+
 if __name__ == '__main__':
-    pass
+    __run()
