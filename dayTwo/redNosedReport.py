0a88c5e46ae28acbc0699115d0685357b9c71a2e
Tony Pham <tony.pham@tc.gc.ca>
2024-12-17 15:44:40 -0500

    Pass the test for getting the correct number of safe reports

--- dayTwo/redNosedReport.py
+++ dayTwo/redNosedReport.py
@@ -1,2 +1,30 @@
-def get_num_safe_reports(reports: list[list[int]]) -> int:
+def __is_adjacent_levels_diff_safe(level: int, adjacent_level: int, min_diff: int, max_diff: int) -> bool:
+    diff = abs(level - adjacent_level)
+    return diff >= min_diff and diff <= max_diff
+
+def __is_report_safe(report: list[int], min_diff: int, max_diff: int) -> bool:
+    isIncreasing = None
+    for i in range(0, len(report) - 1):
+        if report[i] > report[i + 1]:
+            if isIncreasing != None and not isIncreasing:
+                return False
+            isIncreasing = True
+        elif report[i] < report[i + 1]:
+            if isIncreasing != None and isIncreasing:
+                return False
+            isIncreasing = False
+        else:
+            return False
+        if not __is_adjacent_levels_diff_safe(report[i], report[i + 1], min_diff, max_diff):
+            return False
+    return True
+
+def get_num_safe_reports(reports: list[list[int]], min_diff: int, max_diff: int) -> int:
+    num_safe_reports = 0
+    for report in reports:
+        if __is_report_safe(report, min_diff, max_diff):
+            num_safe_reports += 1
+    return num_safe_reports
+
+if __name__ == '__main__':
     pass
--- dayTwo/redNosedReportTest.py
+++ dayTwo/redNosedReportTest.py
@@ -12,9 +12,11 @@ class RedNosedReportTest(unittest.TestCase):
             [8, 6, 4, 4, 1],
             [1, 3, 6, 7, 9]
         ]
+        min_diff = 1
+        max_diff = 3
         expected_num_safe_reports = 2
         # Act
-        sut = get_num_safe_reports(reports)
+        sut = get_num_safe_reports(reports, min_diff, max_diff)
         # Assert
         self.assertEqual(sut, expected_num_safe_reports)
 
