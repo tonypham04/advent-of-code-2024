3d9e9f2e92cc37fdb78909803f391712262a6283
Tony Pham <tony.pham@tc.gc.ca>
2024-12-17 08:44:52 -0500

    Add unit test for advent of code day one

---
+++ dayOneHistorianHysteria.py
@@ -0,0 +1,2 @@
+def sum_list_differences(first_list: list[int], second_list: list[int]) -> int:
+    pass
---
+++ dayOneHistorianHysteriaTest.py
@@ -0,0 +1,16 @@
+import unittest
+from dayOneHistorianHysteria import sum_list_differences
+
+class DayOneHistorianHysteriaTest(unittest.TestCase):
+    def test_sum_list_differences(self):
+        # Arrange
+        first_location_id_list = [3, 4, 2, 1, 3, 3]
+        second_location_id_list = [4, 3, 5, 3, 9, 3]
+        expected_sum = 11
+        # Act
+        sut = sum_list_differences(first_location_id_list, second_location_id_list)
+        # Assert
+        self.assertEqual(sut, expected_sum)
+
+if __name__ == '__main__':
+    unittest.main()
