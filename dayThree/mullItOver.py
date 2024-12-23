1d40ddea7faf0fa251f62adbad9b8b57c2209eb7
Tony Pham <tony.phamx04@gmail.com>
2024-12-23 15:12:11 -0500

    Pass unit test for parsing corrputed data

--- dayThree/mullItOver.py
+++ dayThree/mullItOver.py
@@ -1,5 +1,8 @@
-def parse_corrupted_data_for_muls(corrupted_data: str) -> list[str]:
-    pass
+import re
+
+def parse_corrupted_data_for_muls(corrupted_data: str) -> tuple[str]:
+    pattern = r'(mul\(\d+,\d+\))'
+    return re.findall(pattern, corrupted_data)
 
 def sum_all_muls(muls: list[str]) -> int:
     pass
