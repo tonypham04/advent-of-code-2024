aec2460f3aa225ab9a226b0373836623495e5f09
Tony Pham <tony.phamx04@gmail.com>
2024-12-23 15:28:09 -0500

    Pass the test for sum of all muls

--- dayThree/mullItOver.py
+++ dayThree/mullItOver.py
@@ -5,4 +5,9 @@ def parse_corrupted_data_for_muls(corrupted_data: str) -> tuple[str]:
     return re.findall(pattern, corrupted_data)
 
 def sum_all_muls(muls: list[str]) -> int:
-    pass
+    sum = 0
+    pattern_for_any_int = r'\d+'
+    for mul in muls:
+        xy = re.findall(pattern_for_any_int, mul)
+        sum += int(xy[0]) * int(xy[1])
+    return sum
